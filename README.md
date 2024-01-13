feb7dc5902b4c5b1636e2d17bf0a494532ced6cc
Altay Hodoglugil <altay.hodo@gmail.com>
2024-01-13 14:10:21 -0800

    Create simple task list functionality
    Created the factory function for tasks, the ToDoList module to manage
    the to-do list, with methods to add, remove, toggle tasks. Also a
    DOMHandler module was made to add a simple UI and user interaction.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # to-do-list
+To-do list project with OOP in javascript!
---
+++ script.js
@@ -0,0 +1,71 @@
+function createTask(title){
+  let completed = false;
+  const getCompleted = () => completed;
+  const toggleCompleted = () =>{
+    completed = !completed;
+  };
+  return { title, getCompleted, toggleCompleted}
+}
+
+const task1 = createTask('dishes');
+
+const ToDoList = (function(){
+  let tasks = [];
+
+  function addTask(title){
+    const task = createTask(title);
+    tasks.push(task);
+  }
+
+  function removeTask(index){
+    tasks.splice(index, 1);
+  }
+
+  function toggleTaskCompleted(index){
+    if(tasks[index]){
+      tasks[index].toggleCompleted();
+    }
+  }
+
+  function getTasks(){
+    return tasks;
+  }
+
+  return {
+    addTask,
+    removeTask,
+    toggleTaskCompleted,
+    getTasks,
+  };
+})();
+
+const DOMHandler = (function(){
+  const taskContainer = document.querySelector('.task-list-container');
+
+  function renderTasks(tasks){
+    taskContainer.innerHTML = '';
+    tasks.forEach((task, index) => {
+      const taskItem = document.createElement('div');
+      taskItem.textContent = `${task.title}`;
+      taskContainer.appendChild(taskItem);
+    });
+  }
+  return {renderTasks};
+})();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const addButton = document.querySelector('.add-task-btn');
+  const titleInput = document.querySelector('#task-title');
+  
+  addButton.addEventListener('click', () => {
+    const title = titleInput.value.trim();
+    if(title){
+      ToDoList.addTask(title);
+      titleInput.value = '';
+      DOMHandler.renderTasks(ToDoList.getTasks());
+    } else {
+      alert('Please enter a title.')
+    }
+  });
+})
+
